ef6f87950d87978fb01f68f46ddf5893236ae222
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-10 10:50:34 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -4,49 +4,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Card } from './modules/Card/index';
 import { Menu } from './modules/Menu/index';
-import { MainChart } from './modules/MainChart';
 
 class App extends PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			profiles: null
+			profiles: null,
+			projectActivities: null,
+			managerActivity: null
 		};
 	}
 	componentDidMount() {
-		// call an API to get the team members invloved in the project
-		fetch('https://api.myjson.com/bins/bq60f')
+		// call an API to get the team members invloved in the project, project activities & manager activities
+		fetch('https://api.myjson.com/bins/1358qf')
 			.then(res => res.json())
 			.then(
 				result => {
 					this.setState({
-						profiles: result
+						profiles: result.teamMembers,
+						projectActivities: result.projectActivities
+						// managerActivity: result.managerActivity // TODO
 					});
 				},
 				error => {
-					console.log('User Profiles API fails: ', error);
+					console.log('API failed: ', error);
 				}
 			);
 	}
 
 	render() {
-		console.log(this.state);
-		const profiles = this.state.profiles;
+		const { profiles, projectActivities } = this.state;
 		let currentUser = profiles && profiles[0];
-		console.log(currentUser);
 		return (
 			<div className='App'>
-				<div className='left-container first-layer-padding'>
-					{profiles && Object.keys(profiles).length >= 1 ? <Menu currentUser={currentUser} /> : null}
-				</div>
+				{profiles && Object.keys(profiles).length >= 1 ? <Menu currentUser={currentUser} /> : null}
 				<div className='right-container'>
 					<div className='header inner-padding'>
 						<FontAwesomeIcon icon={faSearch} className='search-icon' />
 						Search...
 					</div>
 					<div className='body-container first-layer-padding'>
-						<Card />
-						<MainChart />
+						<Card data={profiles} type={'Team Members'} className='team-members' />
+						<Card data={projectActivities} type={'Project Activity'} />
+						{/* <Card data={managerActivity} type={'Manager Activity'} /> */}
 					</div>
 				</div>
 			</div>
--- src/modules/MainChart/index.js
+++ src/modules/MainChart/index.js
@@ -1,19 +1,55 @@
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
 import React from 'react';
-// import './App.css';
+import ReactHighcharts from 'react-highcharts';
 
-export const MainChart = () => {
+export const MainChart = props => {
+	const seriesData = [];
+	const colors = ['#EA4E9D', '#6db3f2', '#c4c4c4', '#d3d3d3'];
+	const months = Object.keys(props.data);
+	for (let ctr = 0; ctr < months.length; ctr++) {
+		let monthData = {
+			name: months[ctr],
+			data: props.data[months[ctr]].slice(0, 7),
+			color: colors[ctr]
+		};
+		seriesData.push(monthData);
+	}
+	const config = {
+		chart: {
+			type: 'areaspline'
+		},
+		title: {
+			text: ''
+		},
+		xAxis: {
+			categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+			gridLineWidth: 1
+		},
+		yAxis: {
+			title: {
+				text: ''
+			},
+			gridLineWidth: 0
+		},
+		tooltip: {
+			shared: true,
+			valuePrefix: '$ '
+		},
+		credits: {
+			enabled: false
+		},
+		plotOptions: {
+			areaspline: {
+				fillOpacity: 0.5
+			}
+		},
+		series: seriesData
+	};
+
 	return (
-		<div className='card-container'>
-			<div className='card-heading-wrapper inner-padding'>
-				Projects
-				<div className='icons-wrapper'>
-					<span className='minimize-icon'> - </span>
-					<span className='close-icon'> x </span>
-				</div>
-			</div>
-			<div className='card-body-wrapper inner-padding'>Body</div>
+		<div className='inner-padding'>
+			{/* <div className='chart'> */}
+			<ReactHighcharts config={config} />
+			{/* </div> */}
 		</div>
 	);
 };
