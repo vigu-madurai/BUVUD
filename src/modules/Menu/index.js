6fedb13416ee76246db7201a6a4e80011bc5cf85
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-10 10:50:02 +0530

    css fix

--- src/modules/Menu/index.js
+++ src/modules/Menu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,16 +8,36 @@ export const Menu = props => {
 	const {
 		currentUser: { profile_picture, user_name }
 	} = props;
+	const [hamburger, setHamburger] = useState(window.innerWidth > 1023 ? false : true);
+	if (hamburger) {
+		return (
+			<div className='hamburger-menu first-layer-padding'>
+				<FontAwesomeIcon
+					icon={faBars}
+					className='bars'
+					onClick={() => {
+						setHamburger(!hamburger);
+					}}
+				/>
+			</div>
+		);
+	}
 	return (
-		<>
-			<div className='left-container-heading'>
+		<div className='left-container first-layer-padding'>
+			<div className={`left-container-heading`}>
 				BUVUD
-				<FontAwesomeIcon icon={faBars} className='bars' />
+				<FontAwesomeIcon
+					icon={faBars}
+					className='bars'
+					onClick={() => {
+						setHamburger(!hamburger);
+					}}
+				/>
 			</div>
 			<div className='left-container-user-profile-wrapper'>
 				<img src={profile_picture} className='avatar-img' />
 				{user_name}
 			</div>
-		</>
+		</div>
 	);
 };
