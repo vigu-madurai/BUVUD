acce4209c3458850ecd0d2856ccb25a5c6a8fcfb
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-07-15 01:46:06 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Starts Here -> Load App Component
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
